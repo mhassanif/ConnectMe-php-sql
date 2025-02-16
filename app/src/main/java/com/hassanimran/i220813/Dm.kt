8bd2868fd71345db4666f695a3d75223b06aad07
mhassanif <mhassanimranf@gmail.com>
2025-02-16 13:47:00 +0500

    home screen done

--- app/src/main/java/com/hassanimran/i220813/Dm.kt
+++ app/src/main/java/com/hassanimran/i220813/Dm.kt
@@ -1,32 +1,20 @@
 package com.hassanimran.i220813
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Home : AppCompatActivity() {
+class Dm : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_dm)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        // Initialize and set up the NavBar
-        val navBar = NavBar(this)
-        navBar.setupNavBar()
-
-        val dmButton: Button = findViewById(R.id.dm_button)
-        dmButton.setOnClickListener {
-            val intent = Intent(this, Dm::class.java)
-            startActivity(intent)
         }
     }
 }
