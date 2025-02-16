ea9d093bb817259bb292e386f577e3fa6984de71
mhassanif <mhassanimranf@gmail.com>
2025-02-16 23:10:31 +0500

    Navigation Fixes

--- app/src/main/java/com/hassanimran/i220813/ConfirmPost.kt
+++ app/src/main/java/com/hassanimran/i220813/ConfirmPost.kt
@@ -1,6 +1,8 @@
 package com.hassanimran.i220813
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class ConfirmPost : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val shareButton: Button = findViewById(R.id.share_button)
+        shareButton.setOnClickListener {
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
         }
     }
 }
