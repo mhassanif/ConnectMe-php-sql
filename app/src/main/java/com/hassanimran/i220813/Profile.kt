adba55d5cd6a232bb77e83dd2e0a959dd4c34f46
mhassanif <mhassanimranf@gmail.com>
2025-02-16 19:41:16 +0500

    profile screen done

--- app/src/main/java/com/hassanimran/i220813/Profile.kt
+++ app/src/main/java/com/hassanimran/i220813/Profile.kt
@@ -1,6 +1,9 @@
 package com.hassanimran.i220813
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,23 @@ class Profile : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //add button to lead to follower and follwoing page
+        val followers: LinearLayout = findViewById(R.id.followers)
+        followers.setOnClickListener {
+            val intent = Intent(this, Followers::class.java)
+            startActivity(intent)
+        }
+
+        val following: LinearLayout = findViewById(R.id.following)
+        following.setOnClickListener {
+            val intent = Intent(this, Following::class.java)
+            startActivity(intent)
+        }
+
+        // Initialize and set up the NavBar
+        val navBar = NavBar(this)
+        navBar.setupNavBar()
+
     }
 }
