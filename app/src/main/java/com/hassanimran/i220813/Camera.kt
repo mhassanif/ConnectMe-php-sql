1e536bc77893a7cf02ad3f15b1cd84c5d44d7af9
mhassanif <mhassanimranf@gmail.com>
2025-02-16 22:44:53 +0500

    Camera Page done

--- app/src/main/java/com/hassanimran/i220813/Camera.kt
+++ app/src/main/java/com/hassanimran/i220813/Camera.kt
@@ -1,6 +1,9 @@
 package com.hassanimran.i220813
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +18,18 @@ class Camera : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val next: TextView = findViewById(R.id.next_button)
+        next.setOnClickListener {
+            val intent = Intent(this, ConfirmPost::class.java)
+            startActivity(intent)
+        }
+
+        val gallery: ImageView = findViewById(R.id.last_photo)
+        gallery.setOnClickListener {
+            val intent = Intent(this, NewPost::class.java)
+            startActivity(intent)
         }
     }
 }
