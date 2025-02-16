0b029a7343708ed03554c416092b0616a5dba45e
mhassanif <mhassanimranf@gmail.com>
2025-02-16 06:23:43 +0500

    splash screen done

---
+++ app/src/main/java/com/hassanimran/i220813/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.hassanimran.i220813
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Wait 3 seconds then switch to Login activity
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
