74e0683191e9883f615f6b20b670b7b5bbe74309
mhassanif <mhassanimranf@gmail.com>
2025-02-16 06:39:08 +0500

    navigation bar setup

---
+++ app/src/main/java/com/hassanimran/i220813/NavBar.kt
@@ -0,0 +1,41 @@
+package com.hassanimran.i220813
+
+import android.app.Activity
+import android.content.Intent
+import android.widget.Button
+
+class NavBar(private val activity: Activity) {
+
+    fun setupNavBar() {
+        val homeButton: Button = activity.findViewById(R.id.home_button)
+        val searchButton: Button = activity.findViewById(R.id.search_button)
+        val postButton: Button = activity.findViewById(R.id.post_button)
+        val profileButton: Button = activity.findViewById(R.id.profile_button)
+        val contactButton: Button = activity.findViewById(R.id.contact_button)
+
+        homeButton.setOnClickListener {
+            val intent = Intent(activity, Home::class.java)
+            activity.startActivity(intent)
+        }
+
+        searchButton.setOnClickListener {
+            val intent = Intent(activity, Login::class.java)
+            activity.startActivity(intent)
+        }
+
+        postButton.setOnClickListener {
+            val intent = Intent(activity, Login::class.java)
+            activity.startActivity(intent)
+        }
+
+        profileButton.setOnClickListener {
+            val intent = Intent(activity, Login::class.java)
+            activity.startActivity(intent)
+        }
+
+        contactButton.setOnClickListener {
+            val intent = Intent(activity, Login::class.java)
+            activity.startActivity(intent)
+        }
+    }
+}
